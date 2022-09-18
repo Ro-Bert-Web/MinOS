ba04033b1d7a868f2fc957f7c8648441c225e8c6
Ro-Bert-Web <robertbhs424@gmail.com>
2022-09-18 14:20:49 -0700

    Blinking LED

--- Makefile
+++ Makefile
@@ -1,6 +1,18 @@
-all:
-	arm-none-eabi-gcc -S -o main.s main.c -I ./include
+all: kernel7.elf ;
+
+kernel7.elf: linker.ld start.o
+	arm-none-eabi-gcc -T linker.ld -o kernel7.elf start.o -ffreestanding -nostdlib -lgcc
+
+start.o: start.s
 	arm-none-eabi-as -o start.o start.s
-	arm-none-eabi-as -o main.o main.s
-	arm-none-eabi-gcc -T linker.ld -o kernel7.elf start.o main.o -ffreestanding -nostdlib -lgcc
+
+
+install: all
+	mkdir install
+	cp -r dependencies/* install
 	arm-none-eabi-objcopy kernel7.elf -O binary install/kernel7.img
+	test $(shell ls /media/isaiah | wc -l) -eq 1 && cp -r install/* /media/isaiah/* || echo "Please plug in exactly one drive"
+
+.PHONY: clean
+clean:
+	rm -rf *.o *.elf install
--- start.s
+++ start.s
@@ -13,18 +13,12 @@
 
 _start:
     bl ledInit
-
-loop:
-    sub r0, sp, r15
-    ldr r0, =#0x12
-    bl ledEncode
-
-    ldr r0, =0x1000000
-hold:
-    sub r0, r0, #1
-    cmp r0, #0
-    bne hold
-    b loop
+loop1:
+    bl ledOn
+    bl hold
+    bl ledOff
+    bl hold
+    b loop1
 
 
 ledInit:
@@ -33,64 +27,25 @@ ledInit:
     str r1, [r0, #GPIOFSEL0]
     bx lr
 
-
-ledEncode:
-    ldr r1, =encodeRet
-    str lr, [r1]
-
-encodeLoop:
-    cmp r0, #0
-    beq encodeEnd
-
-    and r1, r0, #0x1
-    cmp r1, #0x1
-    bne short
-    ldr r1, =DELAY1
-    ldr r2, =DELAY3
-    bl ledBlink
-short:
-    ldr r1, =DELAY1
-    ldr r1, =DELAY4
-    bl ledBlink
-
-    lsr r0, r0, #1
-    b encodeLoop
-
-encodeEnd:
-    ldr r1, =encodeRet
-    ldr lr, [r1]
-    bx lr
-
-
-ledBlink:
-    ldr r3, =blinkRet
-    str lr, [r3]
-
-    bl ledOn
-    blinkLoop1:
-        sub r1, r1, #1
-        cmp r1, #0
-        bgt blinkLoop1
-    bl ledOff
-    blinkLoop2:
-        sub r2, r2, #1
-        cmp r2, #0
-        bgt blinkLoop2
-
-    ldr r3, =blinkRet
-    ldr lr, [r3]
-    bx lr
-
-
 ledOn:
     ldr r3, =GPIOBASE
     mov r4, #0x200000
     str r4, [r3, #GPIOSET0]
     bx lr
+
 ledOff:
     ldr r3, =GPIOBASE
     mov r4, #0x200000
     str r4, [r3, #GPIOCLR0]
+    bx lr
+
+hold:
+    ldr r0, =DELAY2
+    and r1, r1, #0
+loop2:
+    sub r0, r0, #1
+    cmp r0, r1
+    bne loop2
     bx lr
 
 
