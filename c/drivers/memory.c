52b641187263ca1b11484a0533923dc74c43ca4b
Ro-Bert-Web <robertbhs424@gmail.com>
2022-09-20 15:10:26 -0700

    Added memcpy

--- c/drivers/memory.c
+++ c/drivers/memory.c
@@ -6,3 +6,12 @@ void write(u32 value, void *addr) {
 u32 read(void *addr) {
     return *(u32*)addr;
 }
+
+void memcpy(void *src, void *dest, u32 n) {
+    char *bsrc = (char*)src;
+    char *bdest = (char*)dest;
+
+    for (int i = 0; i < n; i++) {
+        bdest[i] = bsrc[i];
+    }
+}
