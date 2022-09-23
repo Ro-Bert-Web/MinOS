f9c33e7c12183789366d053a68e856ce28762fed
Ro-Bert-Web <robertbhs424@gmail.com>
2022-09-23 07:09:50 -0700

    Refactored out selecting fselN

--- c/drivers/gpio.c
+++ c/drivers/gpio.c
@@ -2,11 +2,15 @@
 #include "mem.h"
 #include "BCM2837.h"
 
+ptr get_fselN(u32 n) {
+    return BCM2837_GPIO_FSEL0 + n * 4;
+}
+
 void gpio_fsel(u32 port, FSEL mode) {
     u32 fsel = port / 10;
     u32 bitOff = (port % 10) * 3;
 
-    ptr addr = BCM2837_GPIO_BASE + fsel * 4;
+    ptr addr = get_fselN(fsel);
     u32 value = read(addr);
 
     value &= ~(7 << bitOff);
